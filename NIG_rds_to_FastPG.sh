f1f70195822e0109e54c3e8b0c7a06670ccf3273
daiyaohara <ohara.daiya.57x@st.kyoto-u.ac.jp>
2021-06-17 20:23:37 +0900

    Update NIG_rds_to_FastPG.sh

--- NIG_rds_to_FastPG.sh
+++ NIG_rds_to_FastPG.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#$ -l s_vmem=400G -l mem_req=400G
+#$ -l s_vmem=64G -l mem_req=64G
 #$ -N FastPC
+#$ -l short
 
-cd ~/20210613_INF_CD45
-which singularity
 /opt/pkg/singularity/3.7.1/bin/singularity exec ~/fastpg_latest.sif Rscript NIG_rds_to_FastPG.R 
